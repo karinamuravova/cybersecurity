cc3d737a4120eeea4fe58bea46a9148f163ded46
Karina Muravova <168452135+karinamuravova@users.noreply.github.com>
2025-02-25 11:16:35 +0200

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+bool is_prime( int x)
+{
+    bool is_prime=true;
+    for(int i=2;i<=x/2;i++)
+
+    { if (x%i==0)
+        {
+            is_prime=false;
+        }
+
+    }
+
+
+    return is_prime;
+}
+
+
+
+int main(){
+    int x;
+    scanf("%d", &x);
+
+
+    
+    if(is_prime(x))
+    {
+        printf("num prime");
+    }
+    else{
+        printf("num not prime");
+    }
+
+    return EXIT_SUCCESS;
+
+
+}
